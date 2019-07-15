b05b52e26042c29cf70e34afacb4be77175c809d
Lily S <lsturman@redhat.com>
2019-07-15 11:29:12 -0400

    functions for ak and pck

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,21 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use sgx_isa::{Report};
 use dcap_ql::quote::*;
-use std::borrow::Cow;
-use std::env;
+//use std::borrow::Cow;
+//use std::env;
 use std::fs;
-//use openssl::x509::*;
-//use openssl::x509::store::X509StoreBuilder;
-//use openssl::stack::Stack;
-//use openssl::ec::EcKey;
-//use openssl::nid::Nid;
-//use openssl::pkey::PKey;
-//use openssl::sign::Verifier;
-//use openssl::ec::EcPoint;
-//use openssl::bn::BigNumContext;
-//use std::ops::DerefMut;
-//use openssl::ec::EcGroup;
-//use openssl::hash::MessageDigest;
+use openssl::x509::*;
+use openssl::x509::store::X509StoreBuilder;
+use openssl::stack::Stack;
+use openssl::ec::EcKey;
+use openssl::nid::Nid;
+use openssl::pkey::PKey;
+use openssl::sign::Verifier;
+use openssl::ec::EcPoint;
+use openssl::bn::BigNumContext;
+use std::ops::DerefMut;
+use openssl::ec::EcGroup;
+use openssl::hash::MessageDigest;
 
 
 fn handle_client_init(stream_client: TcpStream) {
@@ -76,7 +76,82 @@ fn load_cert(file_path: &str) -> openssl::x509::X509 {
         .expect("Failed to load cert")
 }
 
+// TODO: process chain of arbitrary length
+fn verify_chain_issuers(root_cert: &openssl::x509::X509, 
+                        intermed_cert: &openssl::x509::X509, 
+                        pck_cert: &openssl::x509::X509) {
 
+    assert!(intermed_cert.issued(&pck_cert).as_raw() == 1, 
+        "Intermed cert did not issue leaf cert in PCK chain");
+    
+    assert!(root_cert.issued(&intermed_cert).as_raw() == 1,
+        "Root cert did not issue leaf cert in PCK chain");
+
+    println!("Issuer relationships in PCK cert chain are valid.");
+}
+
+// TODO: process chain of arbitrary length
+fn verify_chain_sigs(root_cert: openssl::x509::X509, 
+                     intermed_cert: openssl::x509::X509, 
+                     pck_cert: &openssl::x509::X509) {
+
+    // create new cert chain object and context
+    let mut chain = Stack::new().unwrap();
+    let mut context = X509StoreContext::new().unwrap();
+    
+    // add root to trusted store
+    let mut store_bldr = X509StoreBuilder::new().unwrap();
+    store_bldr.add_cert(root_cert).unwrap();
+    let store = store_bldr.build();
+    
+    // check that intermediate cert sig checks out in context with root cert
+    assert!(context
+        .init(&store, &intermed_cert, &chain, |c| c.verify_cert())
+        .unwrap());
+
+    // check that pck cert sig fails since intermed cert not yet added to context
+    assert!(!context
+        .init(&store, &pck_cert, &chain, |c| c.verify_cert())
+        .unwrap());
+
+    // add intermed to context chain
+    let _ = chain.push(intermed_cert);
+    
+    // verify pck cert sig in context with intermed cert
+    assert!(context
+        .init(&store, &pck_cert, &chain, |c| c.verify_cert())
+        .unwrap());
+
+    //TODO: check root signature on itself
+}
+
+// TODO: set limit on number of intermediate certs
+// fn check_chain_length {
+//
+// }
+
+fn verify_AK_to_quote(ak: &[u8], report_body: &[u8], report_sig: &[u8]) {
+
+    // set curve and context
+    let ecgroup = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1).unwrap();
+    let mut empty_context = BigNumContext::new().unwrap();
+    let empty_context = empty_context.deref_mut(); 
+
+    // AK transform EcPoint --> EcKey --> PKey --> Verifier
+    let att_key = EcPoint::from_bytes(&ecgroup, ak, empty_context).unwrap();
+    let att_key = EcKey::from_public_key(&ecgroup, &att_key).unwrap();
+    let att_key = PKey::from_ec_key(att_key).unwrap();
+                                        
+    let msgdgst = MessageDigest::from_nid(Nid::X9_62_PRIME256V1).unwrap();
+    let mut att_key_verifier = Verifier::new(msgdgst, &att_key).unwrap();
+
+    // test verifer
+    //att_key_verifier.update(&quote_header[..]).unwrap();
+    //att_key_verifier.update(encl_report_body).unwrap();
+    //assert!(att_key_verifier.verify(&encl_report_sig[..]).unwrap());
+}
+
+                
 fn main() -> std::result::Result<(), std::io::Error> {
     println!("Daemon listening for client request on port 1034... ");
 
@@ -117,79 +192,24 @@ fn main() -> std::result::Result<(), std::io::Error> {
 
                 // TODO: let user choose root cert
 
+                // load certs
                 let pck_cert = load_cert("../pck_cert.pem");
-//
-//                                        // load intermed cert
-//                                        let intermed_cert = include_bytes!("../pck_intermed_cert.pem");
-//                                        let intermed_cert = X509::from_pem(intermed_cert).ok().expect("Failed to load intermed cert");
-//
-//                                        // load root cert
-//                                        let root_cert = include_bytes!("../pck_root_cert.pem");
-//                                        let root_cert = X509::from_pem(root_cert).ok().expect("Failed to load root cert");
-//
-//                                        // check issued relationships
-//                                        let intermed_issued_pck = intermed_cert.issued(&pck_cert);
-//                                        let root_issued_intermed = root_cert.issued(&intermed_cert);
-//                                        println!("Intermed cert issued PCK: {}", intermed_issued_pck);
-//                                        println!("Root cert issued intermed: {}", root_issued_intermed);
-//
-//                                        // check signature on 
-//                                        let _pck_pub_key = pck_cert.public_key();
-//                                        
-//                                        // create cert chain
-//                                        let mut chain = Stack::new().unwrap();
-//
-//                                        // add root to trusted store
-//                                        let mut store_bldr = X509StoreBuilder::new().unwrap();
-//                                        store_bldr.add_cert(root_cert).unwrap();
-//                                        let store = store_bldr.build();
-//
-//                                        // create context to verify cert
-//                                        let mut context = X509StoreContext::new().unwrap();
-//                                        assert!(context
-//                                            .init(&store, &intermed_cert, &chain, |c| c.verify_cert())
-//                                            .unwrap());
-//                                        assert!(context
-//                                            .init(&store, &intermed_cert, &chain, |c| c.verify_cert())
-//                                            .unwrap());
-//
-//                                        // check that untrue verification fails
-//                                        assert!(!context
-//                                                .init(&store, &pck_cert, &chain, |c| c.verify_cert())
-//                                                .unwrap());
-//
-//                                        // add intermed to context chain
-//                                        let _ = chain.push(intermed_cert);
-//
-//
-//                                        // check pck cert verification now
-//                                        assert!(context
-//                                                .init(&store, &pck_cert, &chain, |c| c.verify_cert())
-//                                                .unwrap());
-//                                    
-//                                    
-//                                        println!("PCK cert chain verified");
-//
-//                                        //let pck_pub = EcKey::from_curve_name(Nid::SECP256K1).unwrap();
-//                                        
-//                                        // parameters for AK
-//                                        let ecgroup = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1).unwrap();
-//                                        let mut empty_context = BigNumContext::new().unwrap();
-//                                        let empty_context = empty_context.deref_mut(); 
-//
-//                                        // AK transform EcPoint --> EcKey --> PKey --> Verifier
-//                                        let att_key = EcPoint::from_bytes(&ecgroup, _att_key, empty_context).unwrap();
-//                                        let att_key = EcKey::from_public_key(&ecgroup, &att_key).unwrap();
-//                                        let att_key = PKey::from_ec_key(att_key).unwrap();
-//                                        
-//                                        let msgdgst = MessageDigest::from_nid(Nid::X9_62_PRIME256V1).unwrap();
-//                                        let mut att_key_verifier = Verifier::new(msgdgst, &att_key).unwrap();
-//
-//                                        // test verifer
-//                                        //att_key_verifier.update(&quote_header[..]).unwrap();
-//                                        att_key_verifier.update(encl_report_body).unwrap();
-//                                        assert!(att_key_verifier.verify(&encl_report_sig[..]).unwrap());
-//
+                let intermed_cert = load_cert("../pck_intermed_cert.pem");
+                let root_cert = load_cert("../pck_root_cert.pem");
+                println!("PCK cert chain loaded.");
+                
+                // verify PCK certificate chain
+                let _ = verify_chain_issuers(&root_cert, &intermed_cert, &pck_cert);
+                let _ = verify_chain_sigs(root_cert, intermed_cert, &pck_cert);
+                println!("PCK cert chain verified");
+
+                // verify AK's signature on Quote
+                // verify_AK_to_quote();
+                
+                // verify PCK's signature on AKpub
+                // verify_PCK_to_AK();
+                //let _pck_pub_key = pck_cert.public_key();
+                //let pck_pub = EcKey::from_curve_name(Nid::SECP256K1).unwrap();                           
 
             },
             Err(e) => {
